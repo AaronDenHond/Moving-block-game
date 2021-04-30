beb226e5c059321dba72dcba51bd10e04e75fdfa
Aaron Den Hond <aaron.denhond@gmail.com>
2021-04-30 12:32:54 +0200

    first updates to a snake-like game, index with gameboard, heights and withs set, some css and first function

---
+++ game.js
@@ -0,0 +1,10 @@
+let lastRenderTime = 0;
+
+
+function main(currentTime){
+window.requestAnimationFrame(main);
+ lastRenderTime = currentTime;
+console.log(currentTime);
+}
+
+window.requestAnimationFrame(main)
